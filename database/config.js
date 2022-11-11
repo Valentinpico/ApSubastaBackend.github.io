e8aa65f528b9da30c412e0476e3fb793b7464da8
Valentinpico <86754349+Valentinpico@users.noreply.github.com>
2022-11-11 17:07:24 -0500

    Update config.js

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_CNN)
+        await mongoose.connect(mongodb+srv://valentin:QweQwe23@cluster0.5vgqsxz.mongodb.net/AppSubasta?retryWrites=true&w=majority)
         console.log("base de datos ejecutandose sin problemas")
 
     } catch (error) {
